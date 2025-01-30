167c93cf5c350e969d282206b3552b9906d1d5bf
Akash Gaurav <akash.gaurav@focalyt.com>
2025-01-30 15:21:27 +0530

    member list page

--- controllers/routes/admin/team.js
+++ controllers/routes/admin/team.js
@@ -1,18 +1,49 @@
 const express = require("express");
 const router = express.Router();
 const { isAdmin } = require("../../../helpers");
+const moment = require("moment");
+const { Vacancy } = require("../../models"); 
+const { ObjectId } = require("mongodb");
 
-// Middleware to ensure the user is an admin
+
 router.use(isAdmin);
 
-// Senior Manager page route
+
 router.get("/add", async (req, res) => {
   try {
-    // Fetch any required data here if needed
-    const data = []; // Replace with actual data
-    return res.render(`${req.vPath}/admin/team/add`, { data, menu: 'addTeam', });
-    // return res.render(`${req.vPath}/admin/team/junior`, { data });
+    const data = []; 
+    return res.render(`${req.vPath}/admin/team/add`, { data, menu: "addTeam" });
   } catch (err) {
+    req.flash("error", err.message || "Something went wrong!");
+    return res.redirect("back");
+  }
+});
+
+
+router.get("/view", async (req, res) => {
+  try {
+  
+    const jd = await Vacancy.find({})
+      .populate("_company", "name") 
+      .populate("_qualification", "name") 
+      .lean(); 
+
+    if (!jd || jd.length === 0) {
+      req.flash("error", "No job data found");
+    }
+    let isChecked = req.query.status === "false" ? "true" : "false";
+    const data = {};
+    const totalPages = Math.ceil(jd.length / 20); 
+
+    return res.render(`${req.vPath}/admin/team/teamMember`, {
+      jd,
+      data,
+      moment,
+      totalPages,
+      menu: "team",
+    });
+  } catch (err) {
+    console.error("Error fetching job listings:", err);
     req.flash("error", err.message || "Something went wrong!");
     return res.redirect("back");
   }
