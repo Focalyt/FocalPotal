94f491deafcb678df3e1c44b016f8ffc36a9bd85
Rahul Sharma <rahul.sharma@focalyt.com>
2025-01-30 14:34:12 +0530

    caht now button added

--- controllers/routes/admin/team.js
+++ controllers/routes/admin/team.js
@@ -1,9 +1,52 @@
 const express = require("express");
 const router = express.Router();
 const { isAdmin } = require("../../../helpers");
+// const mongoose = require('mongoose');
+const Team = require('../../models/team'); // PostSchema import करें
+const AWS = require('aws-sdk');
+const multer = require('multer');
+const fs = require('fs');
+const uuid = require('uuid/v1');
+const path = require('path');
 
 // Middleware to ensure the user is an admin
 router.use(isAdmin);
+
+
+
+const {
+  accessKeyId,
+  secretAccessKey,
+  region,
+  bucketName,
+  mimetypes,
+} = require('../../../config');
+
+
+
+AWS.config.update({
+  accessKeyId,
+  secretAccessKey,
+  region,
+});
+
+
+const s3 = new AWS.S3({ region, signatureVersion: 'v4' });
+const allowedImageExtensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp'];
+
+const destination = path.resolve(__dirname, '..', '..', '..', 'public', 'temp');
+if (!fs.existsSync(destination)) fs.mkdirSync(destination);
+
+const storage = multer.diskStorage({
+  destination,
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const basename = path.basename(file.originalname, ext);
+    cb(null, `${basename}-${Date.now()}${ext}`);
+  },
+});
+
+const upload = multer({ storage }).single("file"); // "file" नाम सही होना चाहिए
 
 // Senior Manager page route
 router.get("/add", async (req, res) => {
@@ -17,5 +60,80 @@ router.get("/add", async (req, res) => {
     return res.redirect("back");
   }
 });
+
+router.get("/team", async (req, res) => {
+  try {
+    // Fetch any required data here if needed
+    const data = []; // Replace with actual data
+    return res.render(`${req.vPath}/admin/team/team`, { data, menu: 'team', });
+    // return res.render(`${req.vPath}/admin/team/junior`, { data });
+  } catch (err) {
+    req.flash("error", err.message || "Something went wrong!");
+    return res.redirect("back");
+  }
+});
+
+
+router.post("/add", async (req, res) => {
+  
+    try {
+      const { name, mimetype: ContentType } = req.files.file;
+
+      if (Array.isArray(req.files.file)) {
+        return res.send({
+          status: false,
+          messege: "Only 1 image is allowed"
+        });
+      }
+      const ext = name.split(".").pop();
+      const key = `team/${uuid()}.${ext}`;
+      const data = req.files.file.data;
+
+
+      const params = {
+        Bucket: bucketName,
+        Body: data,
+        Key: key,
+        ContentType
+      };
+      // **Upload to S3**
+      const uploadResult = await s3.upload(params).promise();
+
+      const memberName = req.body.name;
+      const position = req.body.position;
+      const designation = req.body.designation;
+      const description = req.body.description;
+
+
+
+      const newTeam = new Team({
+        name: memberName,
+        image: {
+          fileURL: uploadResult.Location,
+
+        },
+        position,
+        designation,
+        description,
+      });
+
+      const savedTeam = await newTeam.save();
+
+      return res.send({
+        status: true,
+        message: "Team member uploaded successfully",
+        data: savedTeam,
+      });
+
+
+
+    } catch (err) {
+      return req.errFunc(err);
+
+
+
+    }
+
+  });
 
 module.exports = router;
